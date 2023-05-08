2991da538d73d6813f7ea6dd78d69df70ec9623c
EL-R1 <erwan.leblanc.pro@gmail.com>
2023-05-08 19:10:09 +0200

    feat: add Flask Project

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import folium
 import pandas as pd
+from flask import Flask
+from folium.plugins import MarkerCluster
 
-# Chargement des données Opendata de la ville de Nantes pour les arrêts de bus
-arrets_bus = pd.read_csv("C:/Users/mm682/Downloads/stopsCsvTest.csv", sep = ',')
 
-# création d'une carte follium centrée sur Nantes
-m = folium.Map(location=[47.2184, -1.5536], zoom_start=12)
+app = Flask(__name__)
 
-for index, row in arrets_bus.iterrows():
-    folium.Marker(location=[row['stop_lat'], row['stop_lon']],
-                  popup=row['stop_name'],
-                  icon=folium.Icon(color='blue', icon='bus', prefix='fa')
-                 ).add_to(m)
+@app.route("/")
+def tan_map():
+    # Chargement des données Opendata de la ville de Nantes pour les arrêts de bus
+    arrets_bus = pd.read_csv("data/stops.csv", sep = ',')
 
-m.save('C:/Users/mm682/Downloads/carte_arrets.html')
 
+    # création d'une carte follium centrée sur Nantes
+    map = folium.Map(location=[47.2184, -1.5536], zoom_start=12)
+    
+    marker_cluster = MarkerCluster(name='Markers').add_to(map)
+
+    for index, row in arrets_bus.iterrows():
+        if row['location_type']:
+            folium.Marker(location=[row['stop_lat'], row['stop_lon']],
+                      popup=row['stop_name'],
+                      icon=folium.Icon(color='blue', icon='bus', prefix='fa')
+                    ).add_to(marker_cluster)
+
+    folium.LayerControl().add_to(map)
+    map.save('index.html')
+    return map.get_root().render()
+
+
+if __name__ == "__main__":
+    app.run()
