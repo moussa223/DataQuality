c80dd6b6c209ee8d5c5bfd671bce478f4e9d9c6d
EL-R1 <erwan.leblanc.pro@gmail.com>
2023-05-20 23:12:06 +0200

    feat: mapping All datas to display TRAM / BUS / NAVIBUS

--- main.py
+++ main.py
@@ -7,24 +7,46 @@ app = Flask(__name__)
 
 @app.route("/")
 def tan_map():
+    # Importation des différents csv avec uniquement les colonnes intéréssantes.
     stops = pd.read_csv("data/stops.csv", sep = ',')
-    shapes = pd.read_csv('data/shapes.csv')
-    trips = pd.read_csv('data/trips.csv')
-    routes = pd.read_csv('data/routes.csv')
+    shapes = pd.read_csv('data/shapes.csv', usecols=['shape_id', 'shape_pt_lat', 'shape_pt_lon'])
+    trips = pd.read_csv("data/trips.csv", sep=',', usecols=['trip_id', 'route_id', 'shape_id', 'direction_id'])
+    routes = pd.read_csv("data/routes.csv", sep=',', usecols=['route_id', 'route_type', 'route_color'])
+    stop_times = pd.read_csv("data/stop_times.csv", sep=',', usecols=['stop_id', 'trip_id'])
     direction_ids = dict(zip(trips['shape_id'].astype(str), trips['direction_id']))
     route_colors = dict(zip(routes['route_id'].astype(str), routes['route_color']))
-    
+    route_id_type = routes.loc[:, ['route_id', 'route_type']]
+
     map = folium.Map(location=[47.2184, -1.5536], zoom_start=12, tiles='cartodbdark_matter')
 
     marker_cluster = MarkerCluster(name='Markers').add_to(map)
     line_cluster = MarkerCluster(name='Lines').add_to(map)
 
-    for index, stop in stops.iterrows():
-        if stop['location_type']:
+    
+    # Jointure des données pour obtenir les informations nécessaires sur les arrêts enfants et parents
+    merged_stops = pd.merge(stop_times, stops, on='stop_id', how='right')
+    merged_stops = pd.merge(merged_stops, trips, on='trip_id', how='left')
+    merged_stops = pd.merge(merged_stops, routes, on='route_id', how='left')
+
+    # Création d'une nouvelle colonne pour stocker les IDs des arrêts parents correspondants aux arrêts enfants
+    merged_stops['parent_station'] = merged_stops['stop_id'].map(stops.set_index('stop_id')['parent_station'])
+
+    # Filtrage des lignes représentant les arrêts parents
+    parent_stops_merged = merged_stops[merged_stops['parent_station'].notnull()]
+
+    # Supprimer les doublons en conservant uniquement la première occurrence de chaque arrêt
+    unique_stops = parent_stops_merged.drop_duplicates(subset='parent_station', keep='first')
+
+    for index, stop in unique_stops.iterrows():
+        if stop['route_type'] in [0, 3, 4]:
+            icon = 'train' if stop['route_type'] == 0 else 'bus' if stop['route_type'] == 3 else 'ship'
+            color = 'red' if icon == 'train' else 'lightgray' if icon == 'bus' else 'blue'
+
+
             folium.Marker(location=[stop['stop_lat'], stop['stop_lon']],
-                      popup=stop['stop_name'],
-                      icon=folium.Icon(color='blue', icon='bus', prefix='fa')
-                    ).add_to(marker_cluster)
+                        popup=folium.Popup(stop['stop_name']),
+                        icon=folium.Icon(color=color, icon=icon, prefix='fa')
+                        ).add_to(marker_cluster)
 
     for shape_id, group in shapes.groupby('shape_id'):
         points = group[['shape_pt_lat', 'shape_pt_lon']].values.tolist()
@@ -35,6 +57,9 @@ def tan_map():
                 route_id = trip['route_id']
                 if str(route_id) in route_colors:
                     route_color = route_colors[str(route_id)]
+
+
+
                     folium.PolyLine(locations=points, color="#"+route_color).add_to(line_cluster)
     
     folium.LayerControl().add_to(map)
