b8ebf108ef4f5970f1ad15ed9c95828868271ab9
EL-R1 <erwan.leblanc.pro@gmail.com>
2023-05-22 21:35:24 +0200

    feat: change display of markers and clusters

--- main.py
+++ main.py
@@ -3,138 +3,103 @@ import folium
 import pandas as pd
 from flask import Flask
 from folium.plugins import MarkerCluster
+from folium.plugins import FastMarkerCluster
 
 app = Flask(__name__)
 
 @app.route("/")
-# def tan_map():
-#     # Importation des différents csv avec uniquement les colonnes intéréssantes.
-#     stops = pd.read_csv("data/stops.csv", sep = ',')
-#     shapes = pd.read_csv('data/shapes.csv', usecols=['shape_id', 'shape_pt_lat', 'shape_pt_lon'])
-#     trips = pd.read_csv("data/trips.csv", sep=',', usecols=['trip_id', 'route_id', 'shape_id', 'direction_id'])
-#     routes = pd.read_csv("data/routes.csv", sep=',', usecols=['route_id', 'route_type', 'route_color', 'route_short_name', "route_text_color"])
-#     stop_times = pd.read_csv("data/stop_times.csv", sep=',', usecols=['stop_id', 'trip_id'])
-#     direction_ids = dict(zip(trips['shape_id'].astype(str), trips['direction_id']))
-#     route_colors = dict(zip(routes['route_id'].astype(str), routes['route_color']))
+def home():
 
-#     map = folium.Map(location=[47.2184, -1.5536], zoom_start=12, tiles='cartodbdark_matter')
-
-#     marker_cluster = MarkerCluster(name='Markers').add_to(map)
-#     default_cluster = MarkerCluster(name='Lines').add_to(map)
-#     train_cluster = MarkerCluster(name='Tram').add_to(map)
-#     bus_cluster = MarkerCluster(name='Bus').add_to(map)
-#     navibus_cluster = MarkerCluster(name='Navibus').add_to(map)
-    
-#     # Jointure des données pour obtenir les informations nécessaires sur les arrêts enfants et parents
-#     merged_stops = pd.merge(stop_times, stops, on='stop_id', how='right')
-#     merged_stops = pd.merge(merged_stops, trips, on='trip_id', how='left')
-#     merged_stops = pd.merge(merged_stops, routes, on='route_id', how='left')
-
-#     # Création d'une nouvelle colonne pour stocker les IDs des arrêts parents correspondants aux arrêts enfants
-#     merged_stops['parent_station'] = merged_stops['stop_id'].map(stops.set_index('stop_id')['parent_station'])
-
-#     # Filtrage des lignes représentant les arrêts parents
-#     parent_stops_merged = merged_stops[merged_stops['parent_station'].notnull()]
-
-#     # Supprimer les doublons en conservant uniquement la première occurrence de chaque arrêt
-#     unique_stops = parent_stops_merged.drop_duplicates(subset='parent_station', keep='first')
-
-
-#     for index, stop in unique_stops.iterrows():
-#         if stop['route_type'] in [0, 3, 4]:
-#             icon = 'train' if stop['route_type'] == 0 else 'bus' if stop['route_type'] == 3 else 'ship'
-#             color = 'red' if icon == 'train' else 'lightgray' if icon == 'bus' else 'blue'
-#             html = "<body style='background-color:rgb(228,228,228);'><div style='width: 50px; height: 50px; background-color: #"+str(stop['route_color'])+"; color: #"+str(stop['route_text_color'])+"; text-align: center; line-height: 50px; font-size: 20px;'><strong>"+str(stop['route_short_name'])+"</strong></div>"
-
-#             html += "<b>"+str(stop['stop_name'])+"</b><br>"
-#             if stop['wheelchair_boarding'] == 1:
-#                 html += "Accessible aux personnes Handicapées"
-#             html += "</body>"
-#             iframe = folium.IFrame(html=html, width=200, height=200)
-#             folium.Marker(location=[stop['stop_lat'], stop['stop_lon']],
-#                         popup=folium.Popup(iframe),
-#                         icon=folium.Icon(color=color, icon=icon, prefix='fa')
-#                         ).add_to(marker_cluster)
-
-#     for shape_id, group in shapes.groupby('shape_id'):
-#         points = group[['shape_pt_lat', 'shape_pt_lon']].values.tolist()
-
-#         if str(shape_id) in direction_ids and direction_ids[str(shape_id)] == 0:
-#             if str(shape_id) in trips['shape_id'].astype(str).values:
-#                 trip = trips.loc[trips['shape_id'].astype(str) == str(shape_id)].iloc[0]
-#                 route_id = trip['route_id']
-#                 if str(route_id) in route_colors:
-#                     route_color = route_colors[str(route_id)]
-
-#                     route_type = routes.loc[routes['route_id'] == str(route_id), 'route_type'].values[0]
-#                     if route_type == 0:
-#                         line_cluster = train_cluster
-#                     elif route_type == 3:
-#                         line_cluster = bus_cluster
-#                     elif route_type == 4:
-#                         line_cluster = navibus_cluster
-#                     else:
-#                         line_cluster = default_cluster
-
-#                     folium.PolyLine(locations=points, color="#"+route_color).add_to(line_cluster)
-    
-#     folium.LayerControl().add_to(map)
-
-#     map.save('index.html')
-#     return map.get_root().render()
-
-
-def test():
-
+    #Lectures du fichier JSON 
     with open('clear_data.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     map = folium.Map(location=[47.2184, -1.5536], zoom_start=12, tiles='cartodbdark_matter')
-    marker_cluster = MarkerCluster(name='Markers').add_to(map)
-
-    for stop_id, stop in data.items():
-    # Regarde le premier enfant
-        first_child_stop = list(stop['routes'].values())[0]
-
-        if first_child_stop['route_type'] in [0, 3, 4]:
-            icon = 'train' if first_child_stop['route_type'] == 0 else 'bus' if first_child_stop['route_type'] == 3 else 'ship'
-            color = 'red' if icon == 'train' else 'lightgray' if icon == 'bus' else 'blue'
-
-            route_html = "<body style='background-color:rgb(228,228,228);'>"
-            html=""
-            for route in stop['routes'].values():
-                    
-                # Construction de la div pour chaque route
-                html += "<div style='width: 50px; height: 50px; background-color:"+ route['route_color'] +"; color: "+ route['route_text_color'] +"; text-align: center; line-height: 50px; font-size: 20px;'>"+ route['route_short_name'] + "</div>"
-                
-                # Construction de l'HTML pour le point
-                html += "<b>"+ stop['stop_name'] +"</b><br>"
-                if route['wheelchair_boarding'] == 1:
-                    html += "Accessible aux personnes handicapées"
-                else:
-                    html += "PAS Accessible aux personnes handicapées"
-                    
-                html += "</div>"
-            #print(route_html)
-            route_html += html + "</body>"
-            iframe = folium.IFrame(html=route_html, width=300, height=200)
-            
-            # Ajout du marqueur avec le popup dans le cluster
-            folium.Marker(location=[first_child_stop['stop_lat'], first_child_stop['stop_lon']],
-                        popup=folium.Popup(iframe),
-                        icon=folium.Icon(color=color, icon=icon, prefix='fa')
-                        ).add_to(marker_cluster)
-
+    
+    #Lectures des fichiers CSV 
     routes = pd.read_csv("data/routes.csv", sep=',', usecols=['route_id', 'route_type', 'route_color', 'route_short_name', "route_text_color"])
-
     route_colors = dict(zip(routes['route_id'].astype(str), routes['route_color']))
     trips = pd.read_csv("data/trips.csv", sep=',', usecols=['trip_id', 'route_id', 'shape_id', 'direction_id'])
     shapes = pd.read_csv('data/shapes.csv', usecols=['shape_id', 'shape_pt_lat', 'shape_pt_lon'])
     direction_ids = dict(zip(trips['shape_id'].astype(str), trips['direction_id']))
-    marker_cluster = MarkerCluster(name='Markers').add_to(map)
-    default_cluster = MarkerCluster(name='Lines').add_to(map)
-    train_cluster = MarkerCluster(name='Tram').add_to(map)
-    bus_cluster = MarkerCluster(name='Bus').add_to(map)
-    navibus_cluster = MarkerCluster(name='Navibus').add_to(map)
+    
+    #Definition des clusters
+    #Markers
+    marker_default_cluster = FastMarkerCluster(name='Markers', data=[]).add_to(map)
+    marker_train_cluster = FastMarkerCluster(name='Markers - Tram', data=[], options={"disableClusteringAtZoom":13}).add_to(map)
+    marker_bus_cluster = FastMarkerCluster(name='Markers - Bus', data=[], options={"disableClusteringAtZoom":15}).add_to(map)
+    marker_navibus_cluster = FastMarkerCluster(name='Markers - Navibus', data=[], options={"disableClusteringAtZoom":12 }).add_to(map)
+    
+    #Lignes
+    default_cluster = MarkerCluster(name='Lines', ).add_to(map)
+    train_cluster = MarkerCluster(name='Ligne - Tram').add_to(map)
+    bus_cluster = MarkerCluster(name='Ligne - Bus').add_to(map)
+    navibus_cluster = MarkerCluster(name='Ligne - Navibus').add_to(map)
+
+    for stop_id, stop in data.items():
+        first_child_stop = list(stop['routes'].values())[0]
+
+        route_html = "<body style='background-color:rgb(228,228,228);'>"
+        html = ""
+        icons = {'0': 'train', '3': 'bus', '4': 'ship'}
+        colors = {'0': 'red', '3': 'lightgray', '4': 'blue'}
+
+        for route in stop['routes'].values():
+            # Construction de la div pour chaque route
+            html += "<div style='width: 50px; height: 50px; background-color:"+ route['route_color'] +"; color: "+ route['route_text_color'] +"; text-align: center; line-height: 50px; font-size: 20px;'>"+ route['route_short_name'] + "</div>"
+
+            # Construction de l'HTML pour le point
+            html += "<b>"+ stop['stop_name'] +"</b><br>"
+            if route['wheelchair_boarding'] == 1:
+                html += "Accessible aux personnes handicapées"
+            else:
+                html += "PAS Accessible aux personnes handicapées"
+
+            html += "</div><br><br>"
+
+        route_html += html + "</body>"
+        iframe = folium.IFrame(html=route_html, width=300, height=200)
+
+        route_type_dict = {}
+        for route in stop['routes'].values():
+            first_child_stop = list(stop['routes'].values())[0]
+            route_type = route['route_type']
+            # Vérification s'il y a d'autres route_type
+            if route_type != first_child_stop['route_type'] and route_type not in route_type_dict:
+                icon = icons.get(str(route_type), 'question')
+                color = colors.get(str(route_type), 'black')
+                route_type_dict[route_type] = True  # Marquer le route_type comme déjà ajouté
+                if route_type == 0:
+                    marker_cluster = marker_train_cluster
+                elif route_type == 3:
+                    marker_cluster = marker_bus_cluster
+                elif route_type == 4:
+                    marker_cluster = marker_navibus_cluster
+                else:
+                    marker_cluster = marker_default_cluster
+                folium.Marker(
+                        popup=folium.Popup(iframe),
+                        location=[route['stop_lat'], route['stop_lon']],
+                        icon=folium.Icon(color=color, icon=icon, prefix='fa')
+                    ).add_to(marker_cluster)
+
+        # Ajout du marqueur avec le popup dans le cluster pour le premier route_type
+        icon = icons.get(str(first_child_stop['route_type']), 'question')
+        color = colors.get(str(first_child_stop['route_type']), 'black')
+        if first_child_stop['route_type'] == 0:
+            marker_cluster = marker_train_cluster
+        elif first_child_stop['route_type'] == 3:
+            marker_cluster = marker_bus_cluster
+        elif first_child_stop['route_type'] == 4:
+            marker_cluster = marker_navibus_cluster
+        else:
+            marker_cluster = marker_default_cluster
+        folium.Marker(
+            popup=folium.Popup(iframe),
+            location=[first_child_stop['stop_lat'], first_child_stop['stop_lon']],
+            icon=folium.Icon(color=color, icon=icon, prefix='fa')
+        ).add_to(marker_cluster)
+
+
     for shape_id, group in shapes.groupby('shape_id'):
         points = group[['shape_pt_lat', 'shape_pt_lon']].values.tolist()
 
