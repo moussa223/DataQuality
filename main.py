abf8853464e36cfe883ed283ec86d5d110382847
EL-R1 <erwan.leblanc.pro@gmail.com>
2023-05-23 21:31:43 +0200

    refactor: optimise some code + add comments

--- main.py
+++ main.py
@@ -22,27 +22,27 @@ def home():
     shapes = pd.read_csv('data/shapes.csv', usecols=['shape_id', 'shape_pt_lat', 'shape_pt_lon'])
     direction_ids = dict(zip(trips['shape_id'].astype(str), trips['direction_id']))
     
-    #Definition des clusters
+    #Definition des clusters (groupes de lignes ou de points)
     #Markers
-    marker_default_cluster = FastMarkerCluster(name='Markers', data=[]).add_to(map)
+    marker_default_cluster = FastMarkerCluster(name='Markers', data=[], control=False).add_to(map)
     marker_train_cluster = FastMarkerCluster(name='Markers - Tram', data=[], options={"disableClusteringAtZoom":13}).add_to(map)
     marker_bus_cluster = FastMarkerCluster(name='Markers - Bus', data=[], options={"disableClusteringAtZoom":15}).add_to(map)
     marker_navibus_cluster = FastMarkerCluster(name='Markers - Navibus', data=[], options={"disableClusteringAtZoom":12 }).add_to(map)
     
     #Lignes
-    default_cluster = MarkerCluster(name='Lines', ).add_to(map)
+    default_cluster = MarkerCluster(name='Lines', control=False).add_to(map)
     train_cluster = MarkerCluster(name='Ligne - Tram').add_to(map)
     bus_cluster = MarkerCluster(name='Ligne - Bus').add_to(map)
     navibus_cluster = MarkerCluster(name='Ligne - Navibus').add_to(map)
 
     for stop_id, stop in data.items():
-        first_child_stop = list(stop['routes'].values())[0]
-
-        route_html = "<body style='background-color:rgb(228,228,228);'>"
-        html = ""
+        
         icons = {'0': 'train', '3': 'bus', '4': 'ship'}
         colors = {'0': 'red', '3': 'lightgray', '4': 'blue'}
-
+       
+        #Génération du HTML pour chaque arret
+        route_html = "<body style='background-color:rgb(228,228,228);'>"
+        html = ""
         for route in stop['routes'].values():
             # Construction de la div pour chaque route
             html += "<div style='width: 50px; height: 50px; background-color:"+ route['route_color'] +"; color: "+ route['route_text_color'] +"; text-align: center; line-height: 50px; font-size: 20px;'>"+ route['route_short_name'] + "</div>"
@@ -59,12 +59,13 @@ def home():
         route_html += html + "</body>"
         iframe = folium.IFrame(html=route_html, width=300, height=200)
 
+        #Détéction des différents type de points, affichage d'un point pour chaque type de route, bus, tram, navibus, si existant
         route_type_dict = {}
         for route in stop['routes'].values():
-            first_child_stop = list(stop['routes'].values())[0]
             route_type = route['route_type']
+
             # Vérification s'il y a d'autres route_type
-            if route_type != first_child_stop['route_type'] and route_type not in route_type_dict:
+            if route_type not in route_type_dict:
                 icon = icons.get(str(route_type), 'question')
                 color = colors.get(str(route_type), 'black')
                 route_type_dict[route_type] = True  # Marquer le route_type comme déjà ajouté
@@ -81,25 +82,9 @@ def home():
                         location=[route['stop_lat'], route['stop_lon']],
                         icon=folium.Icon(color=color, icon=icon, prefix='fa')
                     ).add_to(marker_cluster)
-
-        # Ajout du marqueur avec le popup dans le cluster pour le premier route_type
-        icon = icons.get(str(first_child_stop['route_type']), 'question')
-        color = colors.get(str(first_child_stop['route_type']), 'black')
-        if first_child_stop['route_type'] == 0:
-            marker_cluster = marker_train_cluster
-        elif first_child_stop['route_type'] == 3:
-            marker_cluster = marker_bus_cluster
-        elif first_child_stop['route_type'] == 4:
-            marker_cluster = marker_navibus_cluster
-        else:
-            marker_cluster = marker_default_cluster
-        folium.Marker(
-            popup=folium.Popup(iframe),
-            location=[first_child_stop['stop_lat'], first_child_stop['stop_lon']],
-            icon=folium.Icon(color=color, icon=icon, prefix='fa')
-        ).add_to(marker_cluster)
-
-
+                
+        
+    #Affichage des lignes
     for shape_id, group in shapes.groupby('shape_id'):
         points = group[['shape_pt_lat', 'shape_pt_lon']].values.tolist()
 
@@ -122,7 +107,7 @@ def home():
 
                     folium.PolyLine(locations=points, color="#"+route_color).add_to(line_cluster)
 
-
+    #Ajout d'un controller pour tous les clusters
     folium.LayerControl().add_to(map)
     map.save('index.html')
     return map.get_root().render()
